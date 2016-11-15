29e67aae46732462b2d0d5e8f10e9ed398a90b8c
dcodeIO <dcode@dcode.io>
2016-11-15 22:39:35 +0100

    Do not generate private definitions if configured, fixed NRE in writeInterfaceForObjectType

--- publish.js
+++ publish.js
@@ -24,6 +24,12 @@ module.exports.publish = function publishTsd(data, opts)
     // remove undocumented stuff.
     data({ undocumented: true }).remove();
 
+    // remove private members if configured.
+    if (opts.private === false)
+    {
+        data({ access: "private" }).remove();
+    }
+
     const docs = data().get();
 
     if (opts.destination === 'console')
@@ -139,8 +145,8 @@ function queueInterfaceForObjectType(element)
 }
 
 function writeInterfaceForObjectType(element)
-{
-    let prefix = env.conf.templates.jsdoc2tsd.interfacePrefix;
+{   
+    let prefix = env.conf.templates.jsdoc2tsd && env.conf.templates.jsdoc2tsd.interfacePrefix;
 
     if (!prefix)
         prefix = '';
